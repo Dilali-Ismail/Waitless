01cd4bcc602a02bc7b08ffad870c1d36dc58e124
Dilaly <ismaildilali@gmail.com>
2026-02-25 10:59:14 +0000

    add init-db.sql

--- docker/init-db.sql
+++ docker/init-db.sql
@@ -1,5 +1,4 @@
--- Créer la base de données pour Keycloak
+
 CREATE DATABASE keycloak_db;
 
--- Donner les permissions à l'utilisateur waitless
 GRANT ALL PRIVILEGES ON DATABASE keycloak_db TO waitless;
