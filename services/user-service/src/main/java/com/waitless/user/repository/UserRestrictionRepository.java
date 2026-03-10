13e5869f97f68f05543b8f714adcea597d72c351
Dilaly <ismaildilali@gmail.com>
2026-03-10 10:58:21 +0000

    creatinf the dtos

--- services/user-service/src/main/java/com/waitless/user/repository/UserRestrictionRepository.java
+++ services/user-service/src/main/java/com/waitless/user/repository/UserRestrictionRepository.java
@@ -2,10 +2,12 @@ package com.waitless.user.repository;
 
 import com.waitless.user.entity.UserRestriction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface UserRestrictionRepository extends JpaRepository<UserRestriction,Integer> {
 
     List<UserRestriction> findByUserIdOrderByAppliedAtDesc(String userId);
