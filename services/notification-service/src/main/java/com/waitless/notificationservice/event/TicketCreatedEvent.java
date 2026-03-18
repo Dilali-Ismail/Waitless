cd2dffa1c1aa30032022cee9b2877c11feae4612
Dilaly <ismaildilali@gmail.com>
2026-03-18 11:02:40 +0000

    notification

---
+++ services/notification-service/src/main/java/com/waitless/notificationservice/event/TicketCreatedEvent.java
@@ -0,0 +1,24 @@
+package com.waitless.notificationservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketCreatedEvent {
+    private String eventType;
+    private Long ticketId;
+    private Long queueId;
+    private String userId;
+    private String clientName;
+    private Integer position;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime timestamp;
+}
