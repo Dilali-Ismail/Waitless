397ed08da132046144cecad9eed370c0e4643a95
Dilaly <ismaildilali@gmail.com>
2026-04-01 13:08:37 +0100

    Fix queue-service CI test config (H2)

--- services/queue-service/queue-service/src/test/java/com/waitless/queueservice/QueueServiceApplicationTests.java
+++ services/queue-service/queue-service/src/test/java/com/waitless/queueservice/QueueServiceApplicationTests.java
@@ -9,7 +9,13 @@ import org.springframework.test.context.TestPropertySource;
         "eureka.client.enabled=false",
         "spring.cloud.discovery.enabled=false",
         "spring.kafka.bootstrap-servers=127.0.0.1:9092",
-        "spring.security.oauth2.resourceserver.jwt.jwk-set-uri=http://localhost:8180/mock-certs"
+        "spring.security.oauth2.resourceserver.jwt.jwk-set-uri=http://localhost:8180/mock-certs",
+        "spring.datasource.url=jdbc:h2:mem:queue_test_db;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;DATABASE_TO_LOWER=TRUE",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class QueueServiceApplicationTests {
 
